898a9ee930490262f670c5a52375d0f7daf4c034
Mohsanul786 <mdmohsan2407@gmail.com>
2025-12-05 17:07:19 +0530

    feat: add pagination functionality

--- src/controllers/submission.controller.js
+++ src/controllers/submission.controller.js
@@ -154,20 +154,41 @@ const createSubmission = async (req, res) => {
 
 const getMySubmissions = async (req, res) => {
   try {
-    const submissions = await Submission.find({ userId: req.user._id }).sort({
-      createdAt: -1,
-    });
+ const page = Number(req.query.page) > 0 ? Number(req.query.page) : 1;
+ const limit = Number(req.query.limit) > 0 ? Number(req.query.limit) : 10;
 
-    return res
-      .status(200)
-      .json(
-        new ApiResponse(200, submissions, "Submissions fetched successfully")
-      );
+ const skip = (page - 1) * limit;
+
+ // get total count for pagination
+ const totalSubmissions = await Submission.countDocuments({
+   userId: req.user._id,
+ });
+
+ const submissions = await Submission.find({ userId: req.user._id })
+   .sort({ createdAt: -1 })
+   .skip(skip)
+   .limit(limit);
+
+ return res.status(200).json(
+   new ApiResponse(
+     200,
+     {
+       submissions,
+       totalSubmissions,
+       totalPages: Math.ceil(
+         totalSubmissions < limit ? 1 : totalSubmissions / limit
+       ),
+       currentPage: page,
+     },
+     "Submissions fetched successfully"
+   )
+ );
   } catch (error) {
     return res
       .status(500)
       .json(new ApiResponse(500, error, "Failed to fetch submissions"));
   }
 };
+
 
 export { createSubmission, getMySubmissions };
